d09bcfeb3c98cc37a2f74f1603ea7e270a8deb1f
Whiskerpillar <Mawwnw@gmail.com>
2025-08-01 15:17:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,20 @@ echo Provision Raspbian System for outernet 1.00
 if mv -f $HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
   echo "File moved successfully."
 else
-  echo "Error: The file could not be moved."
+  echo "Error: outernet-service.sh could not be moved."
   exit 1
 fi
 
 #Setting as exacuatable 
 sudo chmod +x /usr/local/bin/outernet-service.sh
+
+#moving into Systemd
+if mv -f $HOME/outernet/install/service/outernet.service /etc/systemd/system/; then
+  echo "File moved successfully."
+else
+  echo "Error: outernet.service could not be moved."
+  exit 1
+fi
+
+
+
