ed9182e2f6505372267e35d065a59a7de95abd3d
Whiskerpillar <Mawwnw@gmail.com>
2025-08-01 15:26:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,21 @@
 
 echo Provision Raspbian System for outernet 1.00
 
+# Check if the script is being run with sudo
+if [ -z "$SUDO_USER" ]; then
+    echo "This script must be run with sudo."
+    exit 1
+fi
+
+# Get the original user's home directory
+ORIGINAL_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+
+
+
+
 
 #Moving Service from home to usr
-if mv -f $HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
+if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
   echo "File moved successfully."
 else
   echo "Error: outernet-service.sh could not be moved."
@@ -15,7 +27,7 @@ fi
 sudo chmod +x /usr/local/bin/outernet-service.sh
 
 #moving into Systemd
-if mv -f $HOME/outernet/install/service/outernet.service /etc/systemd/system/; then
+if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet.service /etc/systemd/system/; then
   echo "File moved successfully."
 else
   echo "Error: outernet.service could not be moved."
