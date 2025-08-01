da6dba7ea6256061b722835d6cf7b922c98d89ae
Whiskerpillar <Mawwnw@gmail.com>
2025-08-01 11:30:51 -0400

    Create installService.sh

---
+++ install/installService.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo Provision Raspbian System for outernet 1.00
+
+
+#Moving Service from home to usr
+mv ~/outernet/install/service/outernet-service.sh /usr/local/bin/
+
+#Setting as exacuatable 
+sudo chmod +x /usr/local/bin/outernet-service.sh
