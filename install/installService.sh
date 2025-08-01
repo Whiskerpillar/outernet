d91c214242adf8d5897ccd56845382e185710407
Whiskerpillar <Mawwnw@gmail.com>
2025-08-01 11:45:27 -0400

    Update installService.sh

--- install/installService.sh
+++ install/installService.sh
@@ -4,7 +4,15 @@ echo Provision Raspbian System for outernet 1.00
 
 
 #Moving Service from home to usr
-mv -f ~/outernet/install/service/outernet-service.sh /usr/local/bin/
+mkdir 
+
+
+if mv -f $HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
+  echo "File moved successfully."
+else
+  echo "Error: The file could not be moved."
+  exit 1
+fi
 
 #Setting as exacuatable 
 sudo chmod +x /usr/local/bin/outernet-service.sh
