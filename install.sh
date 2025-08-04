9fbbebb7f9cd80be1f993aef159ca8c7faeb4145
Whiskerpillar <Mawwnw@gmail.com>
2025-08-04 11:18:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo Provision Raspbian System for outernet 1.00
+echo Provision Raspbian System for outernet 1.00 
 
 # Check if the script is being run with sudo
 if [ -z "$SUDO_USER" ]; then
@@ -11,9 +11,7 @@ fi
 # Get the original user's home directory
 ORIGINAL_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 
-
-
-
+echo Installing files to system folders...
 
 #Moving Service from home to usr
 if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
@@ -34,5 +32,31 @@ else
   exit 1
 fi
 
+#moving into etc
+if mv -f $ORIGINAL_USER_HOME/outernet/install/outernet.conf /etc/; then
+  echo "File moved successfully."
+else
+  echo "Error: outernet.conf could not be moved."
+  exit 1
+fi
+
+#moving into var/log
+if mv -f $ORIGINAL_USER_HOME/outernet/install/outernet.log /var/log/; then
+  echo "File moved successfully."
+else
+  echo "Error: outernet.conf could not be moved."
+  exit 1
+fi
+echo File move Complete!
+
+LOG_FILE="/var/log/outernet.log"
+
+
+read -p "What is your Node's name? :" setNODE_NAME
+sudo sed "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" /etc/outernet.conf | sudo tee /var/outernet.conf
+sudo sed -i "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" /etc/outernet.conf
+
+read -p "What is your network SSID ? :" setNETWORKSSID
+sudo sed -i "s/WIRELESS_ESSID=.*/WIRELESS_ESSID="$setNETWORKSSID"/" /etc/outernet.conf
 
 
