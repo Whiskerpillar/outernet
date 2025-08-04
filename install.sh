d6c3e11b41e08a1fb8f71adc994f055e38446991
Whiskerpillar <Mawwnw@gmail.com>
2025-08-04 11:59:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-echo Provision Raspbian System for outernet 1.00 
+echo Provision Raspbian System for outernet 1.02
 
-# Check if the script is being run with sudo
+    # Check if the script is being run with sudo
 if [ -z "$SUDO_USER" ]; then
     echo "This script must be run with sudo."
     exit 1
 fi
 
-# Get the original user's home directory
+    # Get the original user's home directory
 ORIGINAL_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 
 read -p "What is your Node's name? :" setNODE_NAME
@@ -17,7 +17,7 @@ read -p "Would you like to use a static IP Address? ('false' disables / IP addre
 read -p "Would you like to install to update and download apps? (y/n) :" installDOWNLOAD_APPS
 echo Installing files to system folders...
 
-#Moving Service from home to usr
+    #Moving Service from home to usr
 if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
   echo "outernet-service.sh: success."
 else
@@ -25,10 +25,10 @@ else
   exit 1
 fi
 
-#Setting as exacuatable 
+    #Setting as exacuatable 
 sudo chmod +x /usr/local/bin/outernet-service.sh
 
-#moving into Systemd
+    #moving into Systemd
 if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet.service /etc/systemd/system/; then
   echo "outernet.service: success."
 else
@@ -36,7 +36,7 @@ else
   exit 1
 fi
 
-#moving into etc
+    #moving into etc
 if mv -f $ORIGINAL_USER_HOME/outernet/install/outernet.conf /etc/; then
   echo "outernet.conf: success."
 else
@@ -44,7 +44,7 @@ else
   exit 1
 fi
 
-#moving into var/log
+    #moving into var/log
 if mv -f $ORIGINAL_USER_HOME/outernet/install/outernet.log /var/log/; then
   echo "outernet.log: success."
   LOG_FILE="/var/log/outernet.log"
@@ -55,14 +55,16 @@ fi
 
 echo Files moved Successfully!
 
+    #Sets location of the Config file
+CONFIG_FILE="/etc/outernet.conf"
 
-#Sends current config to the log
-sudo sed "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" /etc/outernet.conf | sudo tee /var/outernet.conf > /dev/null
+    #Sends current config to the log
+sudo sed "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" $CONFIG_FILE | sudo tee /var/outernet.conf > /dev/null
 
-echo Setting configuration files
-sudo sed -i "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" /etc/outernet.conf
-sudo sed -i "s/WIRELESS_ESSID=.*/WIRELESS_ESSID="$setNETWORKSSID"/" /etc/outernet.conf
-sudo sed -i "s/STATIC_ADDRESS=.*/STATIC_ADDRESS="$setIPADDRESS"/" /etc/outernet.conf
+    echo Setting configuration files
+sudo sed -i "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" $CONFIG_FILE
+sudo sed -i "s/WIRELESS_ESSID=.*/WIRELESS_ESSID="$setNETWORKSSID"/" $CONFIG_FILE
+sudo sed -i "s/STATIC_ADDRESS=.*/STATIC_ADDRESS="$setIPADDRESS"/" $CONFIG_FILE
 
 
 echo config Set Successfully!
