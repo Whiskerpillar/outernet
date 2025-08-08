977568f54671df632308d880836b785eeb91ac2f
Whiskerpillar <Mawwnw@gmail.com>
2025-08-08 10:36:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,22 @@ ORIGINAL_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
 echo Thank you for Installing Outernet | read -t 5 | echo lets keep moving...
 
 read -p "What is your Node's name? :" setNODE_NAME
+setNODE_NAME=${setNODE_NAME:-"Wanderer"}
 read -p "What is your network SSID (default: murmur)? :" setNETWORKSSID
-read -p "Would you like to use a static IP Address? ('false' disables / IP address enables) :" setIPADDRESS
-read -p "Would you like to install to update and download apps? (y/n) :" installDOWNLOAD_APPS
+setNETWORKSSID=${setNETWORKSSID:-"murmur"}
+read -p "Would you like to use a static IP Address? ('false' disables / IP address enables) (default: false) :" setIPADDRESS
+setIPADDRESS=${setIPADDRESS:-"false"}
+read -p "Would you like to install to update and download apps? (y/n) (default: n) :" installDOWNLOAD_APPS
+installDOWNLOAD_APPS=${installDOWNLOAD_APPS:-"n"}
 echo Installing files to system folders...
+
+
+    #Creating and Cleaning old directories
+sudo rm /etc/outernet -r
+mkdir -p /etc/outernet
+
+sudo rm /var/log/outernet
+mkdir -p /var/log/outernet
 
     #Moving Service from home to usr
 if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet-service.sh /usr/local/bin/; then
@@ -27,60 +39,60 @@ else
   exit 1
 fi
 
-    #Setting as exacuatable 
-sudo chmod +x /usr/local/bin/outernet-service.sh
-
-    #moving into Systemd
-if mv -f $ORIGINAL_USER_HOME/outernet/install/service/outernet.service /etc/systemd/system/; then
-  echo "outernet.service: success."
+    #moving Services
+if mv -f $ORIGINAL_USER_HOME/outernet/install/service/services/* /etc/systemd/system/; then
+  echo "Service: success."
 else
-  echo "Error: outernet.service could not be moved."
+  echo "Error: Outerent services could not be moved."
   exit 1
 fi
 
-    #moving into var/log
-if mv -f $ORIGINAL_USER_HOME/outernet/install/outernet.log /var/log/; then
-  echo "outernet.log: success."
+    #moving Log files
+if mv -f $ORIGINAL_USER_HOME/outernet/install/logs/* /var/log/outernet/; then
+  echo "Logs: success."
   LOG_FILE="/var/log/outernet.log"
 else
-  echo "Error: outernet.log could not be moved."
+  echo "Error: log files could not be moved."
   exit 1
 fi
 
-    #Cleaning old files
-sudo rm /etc/outernet -r
-mkdir -p /etc/outernet
-
-    #moving config into etc
+    #moving service config into etc
 if mv -f $ORIGINAL_USER_HOME/outernet/install/service/config/* /etc/outernet/; then
-  echo "outernet.conf: success."
+  echo "Config: success."
 else
   echo "Error: outernet config files could not be moved."
   exit 1
 fi
 
-
 echo Files moved Successfully!
+
+    #Setting as exacuatable 
+sudo chmod +x /usr/local/bin/outernet-service.sh
+
 
     #Sets location of the Config file
 CONFIG_FILE="/etc/outernet/outernet.conf"
 
+
     #Sends current config to the log
 sudo sed "s/NODE_NAME=.*/NODE_NAME="$setNODE_NAME"/" $CONFIG_FILE | sudo tee /var/outernet.conf > /dev/null
 
-    echo Setting configuration files
+echo Setting configuration files
 sudo sed -i "s/^NODE_NAME=.*/NODE_NAME=\"$setNODE_NAME\"/" $CONFIG_FILE
 sudo sed -i "s/^WIRELESS_ESSID=.*/WIRELESS_ESSID=\"$setNETWORKSSID\"/" $CONFIG_FILE
 sudo sed -i "s/STATIC_ADDRESS=.*/STATIC_ADDRESS=\"$setIPADDRESS\"/" $CONFIG_FILE
 
+    #Enabling services. 
 echo Enabling Outernet service
 sudo systemctl enable outernet
 
-echo config Set Successfully!
 
+    #Downloads requried Apps. 
 if [[ "$installDOWNLOAD_APPS" == "y" ]]; then
     sudo apt update
     sudo apt install gpsd
     sudo apt install mumble
     sudo apt install mumble-server
 fi
+
+echo config Set Successfully!
