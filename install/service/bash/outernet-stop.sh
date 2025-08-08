07fbc23ba544225dc754914a38a2da85a3665c5a
Whiskerpillar <Mawwnw@gmail.com>
2025-08-08 15:28:45 -0400

    Update outernet-stop.sh

--- install/service/bash/outernet-stop.sh
+++ install/service/bash/outernet-stop.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-#used for the outernet service to revert it's changes
-
+#Stops Outernet and restores wireless connection
 
 echo Removing old Network Files...
 sudo rm /etc/systemd/network/10-wlan0-mesh.network
 sudo rm /etc/systemd/network/20-bat0.network
 sudo rm /etc/systemd/network/20-bat0.netdev
-echo
+echo Complete!
 
+echo Reloading daemons
 sudo systemctl daemon-reload
+echo Complete!
 
-sudo systemctl stop outernet
+echo Stopping outernet services
+
 sudo systemctl stop outernet-wpa
+sudo systemctl stop outernet
+echo Complete!
 
+Echo Restarting normal network stack
 sudo systemctl restart wpa_supplicant
 sudo systemctl restart systemd-networkd
 sudo systemctl restart NetworkManager
+echo Complete!
+echo Reversion complete
